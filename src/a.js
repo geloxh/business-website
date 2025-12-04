664c4ffb71b4dfd28c9bbdbc2bd1b7bcbd606624
Angelo <ace.blazado7@gmail.com>
2025-12-04 16:40:55 +0800

    initial

--- src/a.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Entry from './Entry.js';
-import './styles.css';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<Entry />);
