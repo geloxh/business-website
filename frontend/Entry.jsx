8989beaf12b8bad85134fe5f1c07a3a2d7cb3504
Angelo <ace.blazado7@gmail.com>
2025-12-04 17:31:06 +0800

    move file

--- frontend/Entry.jsx
+++ frontend/Entry.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './frontend/src/components/Header.jsx';
-import Main from './frontend/src/components/Main.jsx';
-import Footer from './frontend/src/components/Footer.jsx';
+import Header from './src/components/Header.jsx';
+import Main from './src/components/Main.jsx';
+import Footer from './src/components/Footer.jsx';
 
 function Entry() {
     return (
