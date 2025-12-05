a78e48f85470ea65843497e75d3565d366ffcc6c
Angelo <ace.blazado7@gmail.com>
2025-12-05 17:11:58 +0800

    quick update

--- frontend/a.jsx
+++ frontend/a.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Entry from './src/Entry.jsx';
-import './sss/a.css';
-import './sss/tailwind/main.css';
+import './src/styles/sss/a.css';
+import './src/styles/tailwind/main.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
