6f4489dd587beb81682aa6e6ea498be4c6d6d75e
Angelo <ace.blazado7@gmail.com>
2025-12-09 17:11:59 +0800

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import productRoutes from './routes/ptoductRoutes.js';
+import contactRoutes from './routes/contactRoutes.js';
 
 const entry = express();
 const PORT = process.env.PORT || 5000;
 
 entry.use(cors());
 entry.use(express.json());
+
+// Routes
+entry.use('/api/products', productRoutes);
+entry.use('/api/contact', contactRoutes);
 
 entry.get('/api/health', (req, res) => {
     res.json({ message: 'Backend is running' });
