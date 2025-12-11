0d3df0efe9fc222989a7260a57c44f67c9db986c
Angelo <ace.blazado7@gmail.com>
2025-12-11 17:21:11 +0800

    interfaces Product Repository

---
+++ backend/src/domain/interfaces/ProductRepository.js
@@ -0,0 +1,21 @@
+export default class ProductRepository {
+    create(product) {
+        throw new Error("create() not implemented");
+    }
+
+    findAll() {
+        throw new Error("findAll() not implemented");
+    }
+
+    findById(id) {
+        throw new Error("findById() not implemented");
+    }
+
+    update(id, data) {
+        throw new Error("update() not implemented");
+    }
+
+    delete(id) {
+        throw new Error("delete() not implemented");
+    }
+}
